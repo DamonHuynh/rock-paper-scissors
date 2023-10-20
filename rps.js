346af298fc1bff1cea8cc48987cb794e063ba852
Damon Huynh <dhuynh10072007@gmail.com>
2023-10-20 11:36:39 -0700

    add determine winner function
    function takes in the selections and returns who won

--- rps.js
+++ rps.js
@@ -14,3 +14,51 @@ function getComputerChoice()
         return "scissors";
     }
 }
+
+function determineWinner (playerSelection, computerSelection)
+{
+    playerSelection.toLowerCase();
+    computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection)
+    {
+        return "It's a tie!";
+    }
+
+    if (playerSelection == "rock")
+    {
+        if (computerSelection == "scissors")
+        {
+            return "You win, rock beats scissors";
+        }
+        //if computer choice is not scissors, it must be paper
+        else
+        {
+            return "You lose, rock loses to paper"
+        }
+    }
+
+    else if (playerSelection == "scissors")
+    {
+        if (computerSelection == "paper")
+        {
+            return "You win, scissors beats paper";
+        }
+        else
+        {
+            return "You lose, scissors loses to rock"
+        }
+    }
+
+    else if (playerSelection == "paper")
+    {
+        if (computerSelection == "rock")
+        {
+            return "You win, paper beats rock";
+        }
+        else
+        {
+            return "You lose, paper loses to scissors"
+        }
+    }
+}
